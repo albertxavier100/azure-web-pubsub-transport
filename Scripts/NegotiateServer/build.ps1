90d5cf841081c49945d865a3822d8e55d3a8aa98
albertxavier <albertxavier001@icloud.com>
2023-10-07 12:07:23 +0800

    Update docker image build

--- Scripts/NegotiateServer/build.ps1
+++ Scripts/NegotiateServer/build.ps1
@@ -1,2 +1,13 @@
-# e.g. docker build -t awps-negotiate-server:v0.1.1 .  
-docker build -t TAG . 
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$ServerRoot,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$Tag
+)
+# e.g. build.ps1 -ServerRoot "<root>/Resources/NegotiateServersSource~/" -Tag "awps-negotiate-server:v0.1.2"
+
+dotnet clean $ServerRoot
+
+# dotnet build $serverDir
+# docker build -t TAG . 
