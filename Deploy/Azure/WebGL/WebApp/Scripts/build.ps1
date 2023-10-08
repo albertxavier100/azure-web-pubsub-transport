df3d151ab0ba66f61329ba53dd7d8817f7a540f5
albertxavier <albertxavier001@icloud.com>
2023-10-08 11:35:43 +0800

    add build source script

---
+++ Deploy/Azure/WebGL/WebApp/Scripts/build.ps1
@@ -0,0 +1,52 @@
+param(
+    # server
+    [Parameter(Mandatory = $true)]
+    [string]$AzureWebPubSubTransportPackagePath,
+
+    [Parameter(Mandatory = $true)]
+    [string]$ServerOutputPath,
+
+    # unity
+    [Parameter(Mandatory = $true)]
+    [string]$UnityWebGLBuildPath
+)
+# Sample command from your Unity project root
+# .\Packages\com.community.netcode.transport.azure-webpubsub/Deploy\Azure\WebGL\WebApp\Scripts\build.ps1 -AzureWebPubSubTransportPackagePath ".\Packages\com.community.netcode.transport.azure-webpubsub"  -ServerOutputPath "Output~" -UnityWebGLBuildPath "Build\WebGL"
+
+# Source paths
+$ServerPackagePath = "$AzureWebPubSubTransportPackagePath/Resources/NegotiateServersSource~.zip"
+$SharedModelPath = "$AzureWebPubSubTransportPackagePath/Runtime/Models"
+
+# Temp paths
+$ServerTempPath = "$ServerOutputPath/.tmp/Resources/Server"
+$SharedModelTempPath = "$ServerOutputPath/.tmp/Runtime/Models"
+
+# Dest paths
+$WWWRootDestPath = "$ServerOutputPath/wwwroot"
+$DockerfileDestPath = "$ServerOutputPath/Dockerfile"
+
+# Clean output folder
+Remove-Item -Path $ServerOutputPath -Recurse
+New-Item -ItemType "directory" -Path $ServerTempPath
+
+# Extract server package
+Expand-Archive -Path $ServerPackagePath -DestinationPath $ServerTempPath
+
+# Copy shared model
+Copy-Item -Path $SharedModelPath -Destination  $SharedModelTempPath -Recurse
+
+# Generate docker file
+$Dockerfile = @"
+FROM mcr.microsoft.com/dotnet/aspnet:6.0 as runtime
+COPY . .
+ENTRYPOINT ["dotnet", "AWPSNegotiateServer.dll"]
+"@
+Set-Content -Path $DockerfileDestPath -Value $Dockerfile
+
+# Build server
+dotnet clean $ServerTempPath
+dotnet build $ServerTempPath
+dotnet publish $ServerTempPath -o $ServerOutputPath
+
+# Copy Unity build
+Copy-Item -Path $UnityWebGLBuildPath -Destination $WWWRootDestPath -Recurse
---
+++ Export/pre-export.ps1
@@ -0,0 +1,2 @@
+dotnet clean Resources/NegotiateServersSource~
+Compress-Archive -Path "Resources/NegotiateServersSource~/*" -DestinationPath "Resources/NegotiateServersSource~" -Force
